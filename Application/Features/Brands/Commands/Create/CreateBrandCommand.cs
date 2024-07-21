c97647380326f225ca8d43997e47f098f4fd42aa
emrebasar <emrebasar@DESKTOP-JOFQAV0>
2024-07-21 22:34:25 +0300

    Add project files.

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand:IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
+
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        public Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+            createdBrandResponse.Name = request.Name;
+            createdBrandResponse.Id = new Guid();
+            return null;
+        }
+
+        Task<CreatedBrandResponse> IRequestHandler<CreateBrandCommand, CreatedBrandResponse>.Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
